d57b5cf5edc7a9213724c872d2b5b6f55be25439
Augusto-Ildefonso <afildefonso16@gmail.com>
2024-07-26 00:56:41 -0300

    Adicionando o código do evaluation

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "mpc.h"
+#include <string.h>
 
 #ifdef _WIN32
 #include <string.h>
@@ -22,6 +23,43 @@ void add_history(char *unused) {}
 #include <editline/readline.h>
 #include <editline/history.h>
 #endif
+
+long eval(mpc_ast_t *t);
+long eval_op(long x, char *op, long y);
+
+long eval(mpc_ast_t *t){
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  char* op = t->children[1]->contents;
+
+  long x = eval(t->children[2]);
+
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
+
+long eval_op(long x, char *op, long y){
+    if(strcmp(op, "+") == 0){
+        return(x + y);
+    }
+    if(strcmp(op, "-") == 0){
+        return(x - y);
+    }
+    if(strcmp(op, "*") == 0){
+        return(x * y);
+    }
+    if(strcmp(op, "/") == 0){
+        return(x / y);
+    }
+    return(0);
+}
 
 int main (void){
     mpc_parser_t *Number = mpc_new("number");
@@ -46,7 +84,8 @@ int main (void){
         add_history(input);
 
         if(mpc_parse("<stdin>", input, Lispy, &r)){
-            mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         } else{
             mpc_err_print(r.error);
